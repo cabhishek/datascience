8b88a8164b260072d8dd013fc871a7feeb41e14a
Abhishek Kapatkar <cabhishek@gmail.com>
2014-10-05 15:16:23 -0400

    Add data csv files and consolidate neural net

--- neural_network/feed_forward_network.py
+++ neural_network/feed_forward_network.py
@@ -1,17 +1,68 @@
-from analytics.neural_network.race_bins import get_bins
-
 from pybrain.structure import FeedForwardNetwork
 from pybrain.structure import LinearLayer, SigmoidLayer
 from pybrain.structure import FullConnection
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
-from decimal import Decimal as D
-from funcy import first
+from funcy import first, last
 import numpy as np
 import pandas as pd
 import timeit
+import math
 
+BIN_SIZE = 15
+
+def get_bins(race_data):
+    """ Group races and create bins (time ranges) of BIN_SIZE. For each
+        bin find out pct of racers in that bin and avg time of that bin.
+        Also assign bin number to identify racers and their bin they fall
+        into later on.
+    """
+    bin_data = []
+    race_groups = race_data.groupby('race_id')
+
+    for race_id, race_group in race_groups:
+
+        top_75_percentile = race_group[
+            race_group.final_time < race_group.final_time.quantile(.75)]
+
+        # Skip races with missing data.
+        if len(top_75_percentile) == 0:
+            continue
+
+        bins = pd.cut(top_75_percentile.final_time, BIN_SIZE, right=False)
+
+        # fastest = time.strftime(
+        #     '%H:%M:%S', time.gmtime(min(top_75_percentile.final_time)))
+        # slowest = time.strftime(
+        #     '%H:%M:%S', time.gmtime(max(top_75_percentile.final_time)))
+
+        # print "fastest =>", fastest
+        # print "slowest =>", slowest
+
+        bin_number = 0
+
+        for bin_key, bin_group in top_75_percentile.groupby(bins):
+
+            bin_number += 1
+
+            population_pct = len(bin_group) / float(len(top_75_percentile))
+            bin_avg_time = bin_group.final_time.mean()
+
+            if math.isnan(bin_avg_time):
+                # Yes Ugly. Pandas bin key is a string.
+                # This split gives us bin's lower/upper range time.
+                lower_range = float(first(bin_key.split(',')).strip('['))
+                upper_range = float(last(bin_key.split(',')).strip(')'))
+
+                bin_avg_time = np.mean([lower_range, upper_range])
+
+            bin_data.append({'race_id': int(race_id),
+                             'bin_number': bin_number,
+                             'population_pct': population_pct,
+                             'bin_avg_time': bin_avg_time
+                             })
+    return bin_data
 
 def _sum_square_error(actual, desired):
     error = 0.
@@ -87,8 +138,8 @@ def train_network():
     start = timeit.default_timer()
 
     # Read data
-    race_data = pd.read_csv('data/half_ironman_data_v1.csv')
-    race_factors = pd.read_csv('data/half_ironman_race_factors.csv')
+    race_data = pd.read_csv('../data/half_ironman_data_v1.csv')
+    race_factors = pd.read_csv('../data/half_ironman_race_factors.csv')
 
     # Prepare input data
     supervised_dataset = get_supervised_dataset(race_data, race_factors)
